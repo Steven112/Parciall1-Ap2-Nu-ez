9da7823101d9b470fa0fa52e93eee8abc5824035
Steven112 <54567861+Steven112@users.noreply.github.com>
2020-06-06 20:37:21 -0400

    Add project files.

---
+++ Parcial1-Ap2-Nuñez/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Nuñez.DAL
+{
+    public class Contexto : DbContext
+    {
+        // public DbSet<...>...{get ; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\ParcialApicada.db");
+        }
+    }
+
+}
