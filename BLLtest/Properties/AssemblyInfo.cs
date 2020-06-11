a05e19259a2c86b4ab0e43d06c3710f4bff67ee1
Steven112 <54567861+Steven112@users.noreply.github.com>
2020-06-10 20:32:52 -0400

    Model Producto

---
+++ BLLtest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BLLtest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BLLtest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6fac8d4a-2f05-4bcb-91da-42bca5c2823f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Parcial1-Ap2-Nuñez/Models/Producto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Nuñez.Models
+{
+    public class Producto
+    {
+        [Key]
+        [Required(ErrorMessage ="Debe de llenar el campo")]
+        [Range(1,9999999,ErrorMessage ="El id debe de ser mayor a 0 y menor a 9999999")]
+        public int ArticulosId { get; set; }
+
+
+        [Required(ErrorMessage = "Debe de llenar el campo")]
+        [MaxLength(200,ErrorMessage ="La descripcion es muy larga")]
+        [MinLength(2,ErrorMessage ="La descripcion es muy corta")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Debe de llenar el campo")]
+        public int Existencia { get; set; }
+        [Required(ErrorMessage = "Debe de llenar el campo")]
+        public decimal Costo { get; set; }
+        public decimal ValorInventario { get; set; }
+
+        public Producto()
+        {
+            ArticulosId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorInventario = 0;
+        }
+    }
+}
